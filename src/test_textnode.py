b9ec81bdb8bc2ea16c33a6099d18b0b40df50caf
TlexAerry <alexterry987@proton.me>
2025-04-18 14:27:12 +0100

    7. created text node to html node function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType,text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +28,39 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.NormalText)
         node2 = TextNode("This is a text node", TextType.CodeText)
         self.assertNotEqual(node, node2)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_normal_text(self):
+        node = TextNode("This is a text node", TextType.NormalText)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.Image, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BoldText)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_link(self):
+        node = TextNode("this is a link", TextType.Link, "www.website.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag,"a")
+        self.assertEqual(html_node.value, "this is a link")
+        self.assertEqual(html_node.props, {"href":"www.website.com"})
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,12 @@
 from enum import Enum
-
+from htmlnode import LeafNode
 class TextType(Enum):
     NormalText = "normal"
     BoldText = "bold"
     ItalicText = "italic"
     CodeText = "code"
+    Link = "link"
+    Image = "image"
 
 class TextNode():
     def __init__(self, text,type, url = None):
@@ -21,4 +23,19 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text}, {self.type.value}, {self.url})"
 
-        
+def text_node_to_html_node(text_node):
+    match text_node.type:
+        case TextType.NormalText:
+            return LeafNode(None, text_node.text)
+        case TextType.BoldText:
+            return LeafNode("b", text_node.text)
+        case TextType.ItalicText:
+            return LeafNode("i", text_node.text)
+        case TextType.CodeText:
+            return LeafNode("code", text_node.text)
+        case TextType.Link:
+            return LeafNode("a", text_node.text, {"href":f"{text_node.url}"})
+        case TextType.Image:
+            return LeafNode("img","", {"src":f"{text_node.url}","alt":f"{text_node.text}"})
+        case _:
+            raise ValueError(f"{text_node.type} text type is incorrect, please use one of the pre-defined values")
